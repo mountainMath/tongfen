557c0883729ac850610c69626083144f731e5d7f
Jens von Bergmann <jens@mountainmath.ca>
2020-02-22 21:57:37 -0800

    refresh option, fix geo dataset table

--- R/tongfen.R
+++ R/tongfen.R
@@ -203,10 +203,11 @@ aggregate_data_with_meta <- function(data,meta,geo=FALSE,na.rm=TRUE){
 #' @param vectors List of cancensus vectors, can come from different census years
 #' @param geo_format geographic format for returned data, 'sf' for sf format and `NA``
 #' @param quiet suppress download progress output, default is `TRUE`
+#' @param refresh optional character, refresh data cache for this call
 #' for no geographic data, (default `NA`)
 #' @return dataframe with census variables on common geography
 #' @export
-get_tongfen_census_ct <- function(regions,vectors,geo_format=NA,quiet=TRUE) {
+get_tongfen_census_ct <- function(regions,vectors,geo_format=NA,quiet=TRUE,refresh=FALSE) {
   labels="short"
   meta <- meta_for_vectors(vectors)
   geo_datasets <- meta$geo_dataset %>% unique %>% sort
@@ -214,7 +215,7 @@ get_tongfen_census_ct <- function(regions,vectors,geo_format=NA,quiet=TRUE) {
   data <- lapply(geo_datasets,function(g_ds){
     cancensus::get_census(dataset=g_ds,regions=regions,
                           vectors=filter(meta,.data$geo_dataset==g_ds)$variable,
-                          level="CT",geo_format='sf',labels="short",quiet=quiet)
+                          level="CT",geo_format='sf',labels="short",quiet=quiet,use_cache = !refresh)
   })
 
   base <- c("Population","Dwellings","Households")
